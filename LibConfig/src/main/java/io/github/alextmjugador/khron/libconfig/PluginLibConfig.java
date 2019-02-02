946220f1b9347916f52cdd1264cdb7d2bfb5457f
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-02-02 19:31:19 +0100

    Añadir documentación faltante a PluginLibConfig

--- LibConfig/src/main/java/io/github/alextmjugador/khron/libconfig/PluginLibConfig.java
+++ LibConfig/src/main/java/io/github/alextmjugador/khron/libconfig/PluginLibConfig.java
@@ -9,16 +9,23 @@
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package io.github.alextmjugador.khron.libconfig;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Implementa un plugin que provee funcionalidad de gestión de parámetros de
+ * configuración persistentes en almacenamiento secundario, actuando de fachada
+ * para la API de Bukkit.
+ * 
+ * @author AlexTMjugador
+ */
 public final class PluginLibConfig extends JavaPlugin {
     // No queremos hacer nada cuando este plugin se cargue o pare
 }
