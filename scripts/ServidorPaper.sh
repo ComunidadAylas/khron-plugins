0be1dedf5402b242b289a61652e950efe525b84f
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-02-02 19:46:23 +0100

    Mejoras al script de depuración en servidor Paper Ahora permite tener la JVM detenida sin reiniciarse automáticamente o mostrar trazas de la pila de llamadas periódicamente. También se han corregido advertencias que mostraba, ya copia los plugins contenidos en "jar" en el directorio de plugins del servidor y, en general, se ha mejorado su funcionamiento.

--- scripts/ServidorPaper.sh
+++ scripts/ServidorPaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Plugins de Spigot del Proyecto Khron
+# Plugins de Paper del Proyecto Khron
 # Copyright (C) 2019 Comunidad Aylas
 # 
 # This program is free software: you can redistribute it and/or modify
@@ -9,11 +9,11 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
 # 
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 # El URL desde el que se descargará el JAR principal de Paper
 readonly URL_DESCARGA_PAPER=https://papermc.io/ci/job/Paper-1.13/lastSuccessfulBuild/artifact/paperclip.jar
@@ -36,7 +36,11 @@ function instalarPaper {
 
 	# Si tenemos un paperclip.jar, solo lo descargaremos si el ofrecido por el servidor es más reciente que el que tenemos
 	echo "> Descargando Paper..."
-	curl -L -z paperclip.jar -o paperclip.jar -w "%{http_code}" $URL_DESCARGA_PAPER > codigo_http
+	if [ -f 'paperclip.jar' ]; then
+		curl -L -z paperclip.jar -o paperclip.jar -w "%{http_code}" $URL_DESCARGA_PAPER > codigo_http
+	else
+		curl -L -o paperclip.jar -w "%{http_code}" $URL_DESCARGA_PAPER > codigo_http
+	fi
 
 	# Propagar error que pudiese haber ocurrido durante la descarga
 	if [ $? -ne 0 ]; then
@@ -46,11 +50,8 @@ function instalarPaper {
 	fi
 
 	# Si acabamos de descargar el fichero paperclip.jar, realizar tareas de configuración inicial del servidor
-	if [ `cat codigo_http` = 200 ]; then
-		rm codigo_http
+	if [ `cat codigo_http && rm codigo_http` = 200 ]; then
 		echo "> Configurando servidor Paper..."
-		# Aceptar EULA
-		echo 'eula=true' > eula.txt
 		# Aplicar configuraciones predeterminadas del servidor
 		(cat <<SERVER_PROPERTIES
 #Minecraft server properties
@@ -75,7 +76,7 @@ network-compression-threshold=65536
 resource-pack-sha1=
 max-world-size=29999984
 server-port=25565
-server-ip=127.22.12.98
+server-ip=127.0.0.1
 spawn-npcs=true
 allow-flight=false
 level-name=world
@@ -93,6 +94,8 @@ enable-rcon=false
 SERVER_PROPERTIES
 ) > server.properties
 		# Eliminar mensajes verbosos de creación del mundo durante el inicio del servidor
+		# También desactiva el watchdog que reinicia el servidor si tras 60 s no responde,
+		# estableciendo el valor de timeout-time
 		(cat <<SPIGOT_YML
 # This is the main configuration file for Spigot.
 # As you can see, there's tons to configure. Some options may impact gameplay, so use
@@ -116,7 +119,7 @@ settings:
   bungeecord: false
   player-shuffle: 0
   late-bind: false
-  timeout-time: 60
+  timeout-time: 2147483647
   restart-on-crash: true
   restart-script: ./start.sh
   moved-too-quickly-multiplier: 10.0
@@ -223,18 +226,201 @@ world-settings:
       hopper-transfer: 8
       hopper-check: 1
     max-tick-time:
-      tile: 50
-      entity: 50
+      tile: 2147483647
+      entity: 2147483647
 SPIGOT_YML
 ) > spigot.yml
+		# Evitar que Paper llene la salida del servidor de información de traza en caso de que el servidor se congele,
+		# pues ello va a pasar cuando se esté usando el depurador, y es bastante molesto
+		(cat <<PAPER_YML
+# This is the main configuration file for Paper.
+# As you can see, there's tons to configure. Some options may impact gameplay, so use
+# with caution, and make sure you know what each option does before configuring.
+# 
+# If you need help with the configuration or have any questions related to Paper,
+# join us in our Discord or IRC channel.
+# 
+# Discord: https://paperdiscord.emc.gs
+# IRC: #paper @ irc.spi.gt ( http://irc.spi.gt/iris/?channels=paper )
+# Website: https://papermc.io/ 
+# Docs: https://paper.readthedocs.org/ 
+
+verbose: false
+config-version: 17
+settings:
+  player-auto-save-rate: -1
+  max-player-auto-save-per-tick: -1
+  enable-player-collisions: true
+  sleep-between-chunk-saves: false
+  use-versioned-world: false
+  region-file-cache-size: 256
+  save-empty-scoreboard-teams: false
+  incoming-packet-spam-threshold: 300
+  bungee-online-mode: true
+  save-player-data: true
+  queue-light-updates-max-loss: 10
+  suggest-player-names-when-null-tab-completions: true
+  use-alternative-luck-formula: false
+  load-permissions-yml-before-plugins: true
+  async-chunks:
+    enable: true
+    generation: true
+    thread-per-world-generation: true
+    load-threads: -1
+  velocity-support:
+    enabled: false
+    online-mode: false
+    secret: ''
+  watchdog:
+    early-warning-every: 2147483647
+    early-warning-delay: 2147483647
+  book-size:
+    page-max: 2560
+    total-multiplier: 0.98
+  spam-limiter:
+    tab-spam-increment: 1
+    tab-spam-limit: 500
+timings:
+  enabled: true
+  verbose: true
+  server-name-privacy: false
+  hidden-config-entries:
+  - database
+  - settings.bungeecord-addresses
+  history-interval: 300
+  history-length: 3600
+messages:
+  no-permission: '&cI''m sorry, but you do not have permission to perform this command.
+    Please contact the server administrators if you believe that this is in error.'
+  kick:
+    connection-throttle: Connection throttled! Please wait before reconnecting.
+    authentication-servers-down: ''
+    flying-player: Flying is not enabled on this server
+    flying-vehicle: Flying is not enabled on this server
+world-settings:
+  default:
+    disable-thunder: false
+    keep-spawn-loaded-range: 4
+    auto-save-interval: -1
+    keep-spawn-loaded: true
+    disable-ice-and-snow: false
+    skeleton-horse-thunder-spawn-chance: 0.01
+    armor-stands-do-collision-entity-lookups: true
+    fire-physics-event-for-redstone: false
+    skip-entity-ticking-in-chunks-scheduled-for-unload: true
+    allow-non-player-entities-on-scoreboards: false
+    nether-ceiling-void-damage: false
+    water-over-lava-flow-speed: 5
+    use-faster-eigencraft-redstone: false
+    container-update-tick-rate: 1
+    parrots-are-unaffected-by-player-movement: false
+    disable-explosion-knockback: false
+    non-player-arrow-despawn-rate: -1
+    creative-arrow-despawn-rate: -1
+    prevent-tnt-from-moving-in-water: false
+    grass-spread-tick-rate: 1
+    bed-search-radius: 1
+    portal-search-radius: 128
+    fixed-chunk-inhabited-time: -1
+    delay-chunk-unloads-by: 10s
+    queue-light-updates: false
+    max-auto-save-chunks-per-tick: 24
+    enable-treasure-maps: true
+    treasure-maps-return-already-discovered: false
+    optimize-explosions: false
+    use-vanilla-world-scoreboard-name-coloring: false
+    max-chunk-gens-per-tick: 10
+    prevent-moving-into-unloaded-chunks: false
+    save-queue-limit-for-auto-save: 50
+    max-chunk-sends-per-tick: 81
+    armor-stands-tick: true
+    disable-teleportation-suffocation-check: false
+    experience-merge-max-value: -1
+    remove-corrupt-tile-entities: false
+    falling-block-height-nerf: 0
+    tnt-entity-height-nerf: 0
+    spawner-nerfed-mobs-should-jump: false
+    baby-zombie-movement-speed: 0.5
+    allow-leashing-undead-horse: false
+    all-chunks-are-slime-chunks: false
+    mob-spawner-tick-rate: 1
+    duplicate-uuid-resolver: saferegen
+    duplicate-uuid-saferegen-delete-range: 32
+    max-entity-collisions: 8
+    filter-nbt-data-from-spawn-eggs-and-related: true
+    disable-creeper-lingering-effect: false
+    anti-xray:
+      enabled: false
+      engine-mode: 1
+      chunk-edge-mode: 2
+      max-chunk-section-index: 3
+      update-radius: 2
+      hidden-blocks:
+      - gold_ore
+      - iron_ore
+      - coal_ore
+      - lapis_ore
+      - mossy_cobblestone
+      - obsidian
+      - chest
+      - diamond_ore
+      - redstone_ore
+      - lit_redstone_ore
+      - clay
+      - emerald_ore
+      - ender_chest
+      replacement-blocks:
+      - stone
+      - planks
+    game-mechanics:
+      disable-chest-cat-detection: false
+      disable-end-credits: false
+      disable-player-crits: false
+      shield-blocking-delay: 5
+      disable-sprint-interruption-on-attack: false
+      villages-load-chunks: false
+      scan-for-legacy-ender-dragon: true
+      disable-unloaded-chunk-enderpearl-exploit: true
+    max-growth-height:
+      cactus: 3
+      reeds: 3
+    lightning-strike-distance-limit:
+      sound: -1
+      impact-sound: -1
+      flash: -1
+    despawn-ranges:
+      soft: 32
+      hard: 128
+    fishing-time-range:
+      MinimumTicks: 100
+      MaximumTicks: 600
+    frosted-ice:
+      enabled: true
+      delay:
+        min: 20
+        max: 40
+    squid-spawn-height:
+      maximum: 0.0
+    hopper:
+      cooldown-when-full: true
+      disable-move-event: false
+    lootables:
+      auto-replenish: false
+      restrict-player-reloot: true
+      reset-seed-on-fill: true
+      max-refills: -1
+      refresh-min: 12h
+      refresh-max: 2d
+PAPER_YML
+) > paper.yml
 		# Permitir el funcionamiento del comando /restart
 		(cat <<SCRIPT_REINICIO
 #!/bin/bash
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 -jar "paperclip.jar"
+echo Por favor, vuelve a ejecutar el depurador en la configuración de servidor de Paper externo.
+sleep 7
+exit
 SCRIPT_REINICIO
 ) > start.sh
-	else
-		rm codigo_http
 	fi
 	
 	cd ../..
@@ -244,11 +430,44 @@ function arrancarPaper {
 	cd tasks/servidor-paper
 	echo "> Iniciando servidor Paper con soporte para JPDA mediante TCP/IP en el puerto 8000..."
 	echo
-	echo "**********************************************************************"
-	echo "* A PARTIR DE ESTE MOMENTO ES POSIBLE CONECTAR EL DEPURADOR A LA JVM *"
-	echo "************************* (Debug Anyway) *****************************"
+	echo "************************************************************************"
+	echo "* CONECTAR EL DEPURADOR A LA JVM CUANDO SE INDIQUE QUE ESTÁ ESCUCHANDO *"
+	echo "************************* (Debug Anyway) *******************************"
 	echo
-	java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 -jar "paperclip.jar"
+	java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000 -Dcom.mojang.eula.agree=true -jar "paperclip.jar"
+	cd ../..
+	return $?
+}
+
+function copiarPlugins {
+	# Crear directorio donde residirán los plugins del servidor de Paper si hace falta
+	if [ ! -d "tasks/servidor-paper/plugins" ]; then
+		echo "> Creando directorio de plugins del servidor..."
+		mkdir -p tasks/servidor-paper/plugins
+
+		# Propagar error que pudiese haber ocurrido con el sistema de ficheros
+		if [ $? -ne 0 ]; then
+			return $?
+		fi
+	fi
+
+	# Copiar ficheros JAR en el directorio de salida, si hay
+	if [ -d "jar" ]; then
+		echo "> Limpiando directorio de plugins del servidor..."
+		rm -rf tasks/servidor-paper/plugins/*
+
+		# Avisar de los errores que pudiesen haber ocurrido
+		if [ $? -ne 0 ]; then
+			echo "! No se ha podido limpiar el directorio de plugins del servidor de ficheros existentes. Es posible que la depuración parta de un estado inconsistente o difícilmente reproducible."
+		fi
+
+		echo "> Copiando plugins empaquetados al directorio de plugins del servidor..."
+		cp jar/*.jar tasks/servidor-paper/plugins
+
+		if [ $? -ne 0 ]; then
+			echo "! Ha ocurrido un error al copiar los plugins empaquetados al directorio de plugins del servidor. Es muy posible que no se hayan colocado y, por tanto, no se carguen."
+		fi
+	fi
 }
 
 echo "*************************************************************************************"
@@ -260,5 +479,22 @@ echo
 # Instalar Paper, si no lo está, y si todo va bien abrir el servidor
 instalarPaper
 if [ $? -eq 0 ]; then
+	copiarPlugins
 	arrancarPaper
+	if [ $? -eq 0 -o -f "tasks/servidor-paper/paperclip.jar" ]; then
+		echo "! Ha ocurrido un error arrancando el servidor de Paper. Esto puede deberse a un fichero paperclip.jar corrupto. ¿Quieres descargarlo de nuevo? (S/N) "
+		read r
+		case "$r" in
+			S|s)
+				clear
+				rm tasks/servidor-paper/paperclip.jar
+				instalarPaper
+				if [ $? -eq 0 ]; then
+					copiarPlugins
+					arrancarPaper
+				fi;;
+			*)
+				;;
+		esac
+	fi
 fi
